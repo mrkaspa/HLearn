e75e27b1c04b93a70e24c76472a9014af4f504a9
michel perez <michel.ingesoft@gmail.com>
2017-07-27 15:25:08 -0500

    adding twilio

---
+++ app/TwilioMain.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Control.Monad.IO.Class (liftIO)
+import System.Environment (getEnv)
+import Twilio
+import Twilio.Messages
+
+main :: IO ()
+main =
+  runTwilio' (getEnv "TWILIO_ACCOUNT_SID") (getEnv "TWILIO_AUTH_TOKEN") $ do
+    let body = PostMessage "NUMBER_TO_TEXT" "YOUR_TWILIO_NUMBER" "Hello, World!"
+    message <- post body
+    liftIO $ print message
