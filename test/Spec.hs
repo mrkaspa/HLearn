42d25728ff8cba365c9cae6edbed61e783eb2c7f
Michel Perez <michel.ingesoft@gmail.com>
2017-07-26 22:27:36 -0500

    wip

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,9 +7,7 @@ main =
   hspec $ do
     describe "sumiz" $ do
       it "should be Nothing" $ sumiz Nothing (Just 3) `shouldBe` Nothing
-      it "should be 5" $ do
-        res <- return $ sumiz (Just 3) (Just 2)
-        res `shouldBe` Just 5
+      it "should be 5" $ do (sumiz (Just 3) (Just 2)) `shouldBe` Just 5
     describe "props" $ do
       it "x + 1 is always greater than x" $ do
         property $ \x -> x + 1 > (x :: Int)
