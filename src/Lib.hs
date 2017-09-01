b630ce9de501b0a0a97a6b97d037a0762ece6522
michel perez <michel.ingesoft@gmail.com>
2017-09-01 15:41:38 -0500

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,21 +15,15 @@ printNewName uname =
     -- putStrLn $ (name new) ++ " --- " ++ (show . age $ new)
   in print new
 
-sumi
-  :: (Fractional a, Eq a)
-  => a -> a -> a
+sumi :: (Fractional a, Eq a) => a -> a -> a
 sumi = (+)
 
-divi
-  :: (Fractional a, Eq a)
-  => a -> a -> a
+divi :: (Fractional a, Eq a) => a -> a -> a
 divi a b
   | b == 0 = 0
   | otherwise = a / b
 
-divi2
-  :: (Fractional a, Eq a)
-  => a -> a -> Maybe a
+divi2 :: (Fractional a, Eq a) => a -> a -> Maybe a
 divi2 a 0 = Nothing
 divi2 a b = Just (a / b)
 
@@ -48,44 +42,41 @@ printi a = putStr a
 
 parsiPrinti = printi . parsi
 
-sumiMonad
-  :: (Num a)
-  => Maybe a -> Maybe a -> Maybe a
+sumiMonad :: (Num a) => Maybe a -> Maybe a -> Maybe a
 sumiMonad (Just a) (Just b) = Just (a + b)
 sumiMonad _ _ = Nothing
 
-sumiMonad1
-  :: (Num a)
-  => Maybe a -> Maybe a -> Maybe a
+sumiMonad1 :: (Num a) => Maybe a -> Maybe a -> Maybe a
 sumiMonad1 a b = do
   a' <- a
   b' <- b
   return (a' + b')
 
-sumList
-  :: Num t
-  => [t] -> t
+sumList :: Num t => [t] -> t
 sumList [] = 0
 sumList (h:t) = h + sumList (t)
 
-sumList1
-  :: Num t
-  => [t] -> t -> t
+sumList1 :: Num t => [t] -> t -> t
 sumList1 [] acc = acc
 sumList1 (h:t) acc = sumList1 t (acc + h)
 
-sumList2
-  :: Num t
-  => [t] -> t
+sumList2 :: Num t => [t] -> t
 sumList2 listi =
   let sumList2' [] acc = acc
       sumList2' (h:t) acc = sumList2' t (h + acc)
   in sumList2' listi 0
 
-sumList3
-  :: Num t
-  => [t] -> t
+sumList3 :: Num t => [t] -> t
 sumList3 listi = sumList2' listi 0
   where
     sumList2' [] acc = acc
     sumList2' (h:t) acc = sumList2' t (h + acc)
+
+inc :: (Integer -> Integer) -> [Integer] -> [Integer]
+inc incFn ls = map incFn ls
+
+incBy1 :: [Integer] -> [Integer]
+incBy1 ls = inc ((+) 1) ls
+
+incBy2 :: [Integer] -> [Integer]
+incBy2 = inc ((+) 1)
