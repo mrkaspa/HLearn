7f7b15a5157a90583502e21d3295bcea4d721daf
Michel Perez <michel.ingesoft@gmail.com>
2018-04-16 21:28:59 -0500

    wip format

--- src/Employee.hs
+++ src/Employee.hs
@@ -10,10 +10,8 @@ data Employee
 reportBoss :: Employee -> Employee -> IO ()
 reportBoss e e' = putStrLn $ show e ++ " is the boss of " ++ show e'
 
-employeeRank :: (Employee -> Employee -> Ordering)
-             -> Employee
-             -> Employee
-             -> IO ()
+employeeRank ::
+     (Employee -> Employee -> Ordering) -> Employee -> Employee -> IO ()
 employeeRank f e e' =
   case f e e' of
     GT -> reportBoss e e'
